341c2b165b964f5faf06ce4e35256bc0ecbac261
Ferrer, Alastair <ferreralastair30@gmail.com>
2025-10-01 23:52:49 +0800

    commit 13 "change logo and title of website"

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,10 +2,7 @@ import PageRouter from './container/routes/PageRouter.jsx'
 function App() {
 
   return (
-    <>
-      <title> Barangay Resident Master List Monitoring System</title>
         <PageRouter />
-    </>
   )
 }
 
