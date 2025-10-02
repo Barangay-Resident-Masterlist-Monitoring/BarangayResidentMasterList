2751efbd3b9687bd5841a415ab88b9d88df0ac22
Ferrer, Alastair <ferreralastair30@gmail.com>
2025-10-03 06:56:35 +0800

    commit 17 "minor fix"

--- front-end/src/container/routes/PageRouter.jsx
+++ front-end/src/container/routes/PageRouter.jsx
@@ -1,7 +1,4 @@
-
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-
-
 
 import Resident from "../resident/main.jsx";
 import ResidentLogin from "../resident/login.jsx";
@@ -9,39 +6,35 @@ import ResidentDashboard from "../resident/dashboard.jsx";
 import RegisterResidents from "../resident/RegisterResident.jsx";
 import GenerateReports from "../resident/generate_reports.jsx";
 
-
 import Secretary from "../secretary/main.jsx";
 import SecretaryLogin from "../secretary/login.jsx";
 import SecretaryDashboard from "../secretary/dashboard.jsx";
 import ManageResidentsAndOfficial from "../secretary/ManagerUser.jsx"; 
 
-
 const PageRouter = () => {
   return (
-      <Router>
-        <Routes>
-         <Route path="/resident/login" element={<ResidentLogin />} />
-          <Route path="/secretary/login" element={<SecretaryLogin />} />
-          <Route path="/*" element={<Navigate to="/resident/login" />} />
+    <Router>
+      <Routes>
+        <Route path="/resident/login" element={<ResidentLogin />} />
+        <Route path="/secretary/login" element={<SecretaryLogin />} />
 
+        <Route path="/resident" element={<Resident />}>
+          <Route path="dashboard" element={<ResidentDashboard />} />
+          <Route path="register-residents" element={<RegisterResidents />} />
+          <Route path="generate-report" element={<GenerateReports />} />
+          <Route index element={<Navigate to="dashboard" replace />} />
+        </Route>
 
-          <Route 
-            exact path="/resident"
-            element={ <Resident /> }>
-                <Route path="dashboard" element={<ResidentDashboard  />} />
-                <Route path="register-residents" element={<RegisterResidents  />} />
-                <Route path="generate-report" element={<GenerateReports  />} />
-            </Route>
+        <Route path="/secretary" element={<Secretary />}>
+          <Route path="dashboard" element={<SecretaryDashboard />} />
+          <Route path="manage-residents" element={<ManageResidentsAndOfficial />} />
+          <Route index element={<Navigate to="dashboard" replace />} />
+        </Route>
 
-          <Route 
-           exact path="/secretary"
-            element={<Secretary />}>
-                <Route path="dashboard" element={<SecretaryDashboard />} />
-                <Route path="manage-residents" element={<ManageResidentsAndOfficial/>} />
-            </Route>
-        </Routes>
-      </Router>
-  )
-}
+        <Route path="*" element={<Navigate to="/resident/login" replace />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default PageRouter
+export default PageRouter;
